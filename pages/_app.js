bcb6a3ea7c5a23632b11ecdeafa80d9b5f295c47
Ruth-Retta <ruthretta2000@gmail.com>
2024-08-01 17:21:20 +0300

    admin page worked

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import 'swiper/css';
 import 'swiper/css/navigation';
 import '../styles/globals.css';
+import { SessionProvider } from 'next-auth/react';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
 
 export default MyApp;
